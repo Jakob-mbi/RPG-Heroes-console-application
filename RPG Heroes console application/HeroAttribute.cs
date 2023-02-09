ae8c8bf99e2164fb51386b361da5bdd845a72690
Jakob-mbi <jakob_mbi@msn.com>
2023-02-09 12:49:43 +0100

    feat: HeroAttribute class

---
+++ RPG Heroes console application/HeroAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Heroes_console_application
+{
+    internal class HeroAttribute
+    {
+        public int Strength { get; private set; }
+        public int Dexterity { get; private set; }
+        public int Intelligence { get; private set; }
+
+
+
+
+        public HeroAttribute(int strength, int dexterity, int intelligence)
+        {
+            this.Strength = strength;
+            Dexterity = dexterity;
+            Intelligence = intelligence;
+        }
+
+        public void LevelUpAttribtues(int strength, int dexterity, int intelligence)
+        {
+            this.Strength += strength;
+            Dexterity += dexterity;
+            Intelligence += intelligence;
+        }
+        public int AttribtuesSum()
+        {
+            int sum = Strength+Dexterity+Intelligence;
+
+            return sum;
+        }
+
+
+    }
+}
