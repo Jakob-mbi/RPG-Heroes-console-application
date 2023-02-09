ce7795eed2ef5d5fbcd7074762801694552e8c4c
Jakob-mbi <jakob_mbi@msn.com>
2023-02-09 09:37:44 +0100

    Add project files.

---
+++ RPG Heroes console application/Heros/Ihero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Heroes_console_application.Heros
+{
+    internal interface Ihero
+    {
+        string name { get; set; }
+
+        int level { get; set; }
+        int LevelAttribtues { get; set; }
+
+        string Equipment { get; set; }
+
+        string ValidWeaponTypes { get; set; }
+
+        string ValidArmorTypes { get; set; }
+
+        public void LevelUp();
+        public void Equip();
+        public void Damage();
+        public void TotalAttributes();
+        public void Display();
+
+    }
+}
