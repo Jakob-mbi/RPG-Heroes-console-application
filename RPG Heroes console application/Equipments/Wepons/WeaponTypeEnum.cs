34038aaf734a41ddd89e673d0e3150a8028c643e
Jakob-mbi <jakob_mbi@msn.com>
2023-02-09 12:54:04 +0100

    feat: Wepon class and enum

---
+++ RPG Heroes console application/Equipments/Wepons/WeaponTypeEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Heroes_console_application.Equipments.Wepons
+{
+    internal enum WeaponTypeEnum
+    {
+        Axes,
+        Bows,
+        Daggers,
+        Hammers,
+        Staffs,
+        Swords,
+        Wands
+    }
+}
---
+++ RPG Heroes console application/Equipments/Wepons/WeponsClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Heroes_console_application.Equipments.Wepons
+{
+    internal class WeponsClass : EquipmentBaseClass
+    {
+        public WeponTypeEnum WeponType { get; set; }
+
+        public WeponsClass(string name, int requiredLevel, SlotEnum slot) : base(name, requiredLevel, slot)
+        {
+            this.Name = name;
+            RequiredLevel= requiredLevel;
+            Slot= slot;
+        }
+    }
+}
