ed5177382f0b286d6f4a42f6f0130d016aee43d7
Jakob-mbi <jakob_mbi@msn.com>
2023-02-10 14:53:30 +0100

    feat: InvalidArmorException and InvalidWeponException

---
+++ RPG Heroes console application/EquipmentItem/WeponsItem/InvalidWeaponException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Heroes_console_application.EquipmentItem.WeponsItem
+{
+    internal class InvalidWeaponException :Exception
+    {
+         public InvalidWeaponException() { }
+         public InvalidWeaponException(string message) : base(message) { }
+    }
+}
